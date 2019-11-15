8aa5fe81080a3c12126e11a09e26c9bcac2e706e
Gizdich <john.gizdich@utexas.edu>
2019-11-14 20:27:36 -0600

    Adder Testbench

--- lab_6.srcs/sim_1/new/AddTest0.v
+++ lab_6.srcs/sim_1/new/AddTest0.v
@@ -25,11 +25,29 @@ module AddTest0();
     reg [7:0] B;
     wire [7:0] Q;
     
-    Acc adder(A, B, Q);
+    Acc0 adder(A, B, Q);
     
     initial begin
         A = 8'b01001100; // x4C
         B = 8'b00110111; // x37
+        #100;
+        A = 8'b11100111; // xE7
+        B = 8'b00011000; // x18 // Add with big negative number
+        #100;
+        A = 8'b01101000; // x68
+        B = 8'b01101000; // x68 // Overflow test
+        #100;
+        A = 8'b11101000; // xE8
+        B = 8'b11101000; // xE8 // Underflow test
+        #100;
+        A = 8'b11111000; // xF8
+        B = 8'b11101000; // xE8 // 111 exp test case
+        #100;
+        A = 8'b10001000; // x88
+        B = 8'b11101000; // xE8 // 000 exp test case
+        #100;
+        A = 8'b00000000; // x00
+        B = 8'b00000000; // x00 // Another 000 exp test case
         #100;
     end
 endmodule
